8d66a820c4c647326b5e106af0ffec409a1c4127
sachin6969 <115332096+sachinidfc69@users.noreply.github.com>
2023-08-03 11:30:29 +0545

    Update pointer.c

--- pointer.c
+++ pointer.c
@@ -4,6 +4,8 @@ void swap(int*pa, int *pb){
 	int temp = *pa;
 	*pa = *pb;
 	*pb = temp;
+	
+	printf("now the value of a = %d and b = %d\n",a,b);
 }
 
 void main(){
@@ -12,5 +14,5 @@ void main(){
 	printf("before the value of a = %d and b = %d\n",a,b);
 	swap(&a,&b);
 	
-	printf("now the value of a = %d and b = %d\n",a,b);
+	
 }
