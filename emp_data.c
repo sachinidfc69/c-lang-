8478ca5c29ca6127b0be4748e18f068d5cae1753
sachin6969 <115332096+sachinidfc69@users.noreply.github.com>
2023-08-07 12:11:57 +0545

    Update emp_data.c

--- emp_data.c
+++ emp_data.c
@@ -5,24 +5,35 @@ int main(){
 	char name[100];
 	int age,i;
 	char input;
+	// --- > part 1;
 	do{
 		printf("enter your name : ");
 		scanf("%s",name);
 		printf("entry your age : ");
 		scanf("%d",&age);
+		
 		fprintf(pfile,"name = %s and age = %d\n",name,age);
-		printf("do you still wanna input data the enter 'y' else 'n' ");
-		scanf("%c",&input);
-	}while(input == 'y');
+		
+		printf("do you still wanna input data then enter 'y' else 'n': ");
+		scanf("%s",&input);
+		
+//		printf("the value of input = %c\n ",input);
+
+		}while(input == 'y');
+		
+		// ---> part 2;
+		
+//	int input1;
+//	printf("enter the total number of data : ");
+//	sscanf("%d",&input1);
 	
-//	for(i=1;i<=input;i++){
+//	for(i=1;i<=input1;i++){
 //		printf("enter your name : ");
 //		scanf("%s",name);
 //		printf("entry your age : ");
 //		scanf("%d",&age);
 //		fprintf(pfile,"name = %s and age = %d\n",name,age);
-//		printf("do you still wanna input data the enter 'y' else 'n' ");
-//		scanf("%c",&input);
+//		
 //	}
 	
 	fclose(pfile);
