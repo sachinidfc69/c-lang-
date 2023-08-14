b8d1b725f0f798a65ef46910a3f5fec3b40557a9
sachin6969 <115332096+sachinidfc69@users.noreply.github.com>
2023-08-14 11:21:00 +0545

    Add files via upload

---
+++ emp_data_p2.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+int main(){
+	
+	FILE *pfile = fopen("data.txt","w");
+	
+	int n, age , id;
+	char name[100];
+	
+	printf("enter the n value");
+	scanf("%d",&n);
+	int i;
+	if(pfile == NULL){
+		printf("file can't be opened");
+	}else{
+	
+	for(i=0;i<n;i++){
+		printf("enter the name : ");
+		scanf("%s",name);
+		printf("enter your id : ");
+		scanf("%d",&id);
+		printf("enter your age : ");
+		scanf("%d",&age);
+		fprintf(pfile,"%s\t %d\t %d\n",name ,id,age);
+		
+		
+	}
+	fclose(pfile);
+	}
+	pfile = fopen("data.txt","r");
+	
+	while(fscanf(pfile,"%s\t %d\t %d",name,&id,&age) != EOF){
+		printf("name = %s \t, id = %d\t and age = %d\n",name,id,age);
+	}
+	
+	fclose(pfile);
+	return 0;
+}
